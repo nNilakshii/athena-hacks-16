88aa02a0a233f888ad0951a1814257dce5edf1c9
Nilakshi Nagrale <nagrale@usc.edu>
2025-03-02 09:08:09 -0800

    added login api

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -154,4 +154,34 @@ router.get("/recommend-buddies",  async (req, res) => {
   });
 
 
+// LOGIN USER
+// Login Route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    // Generate JWT Token
+    const payload = { user: { id: user.id } };
+    const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "1h" });
+
+    // Send token as response
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
