1163ca4fd18aa90c7135de0d2d9f7d1e3d86036f
Nilakshi Nagrale <nagrale@usc.edu>
2025-03-02 02:56:40 -0800

    gemini integration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,3 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception:", err);
+  });
+  
+  process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Promise Rejection:", err);
+  });
+  
